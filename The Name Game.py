d180a6247ef0242881febab7fd6ff396512d7ad0
Ithian <ithian@hotmail.com>
2018-02-09 21:38:19 -0600

    Create The Name Game.py

---
+++ The Name Game.py
@@ -0,0 +1,6 @@
+gameName = input()
+
+print((gameName + chr(44) + chr(32)) * 2 + ("bo-b" + gameName[1:len(gameName)]))
+print("banana-fana fo-f" + gameName[1:len(gameName)])
+print("fee-fi-mo-m"  + gameName[1:len(gameName)])
+print(gameName + "!")
